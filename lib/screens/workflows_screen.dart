9eb76ec212e5f1f13a3d80578f6441476f91f8bc
JasperManousek <JasperManousek@users.noreply.github.com>
2026-07-28 12:48:21 +0200

    basic structure

---
+++ lib/screens/workflows_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorkflowsScreen extends StatelessWidget {
+  const WorkflowsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Workflows',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
